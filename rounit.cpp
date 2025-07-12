d35dc28ce58bbe3af5fa9b4a68e6115b265d19ba
rounit568 <rounitkashyap568@gmail.com>
2025-07-12 23:02:55 +0530

    DAY 1

---
+++ rounit.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    // Taking input from user
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Adding the numbers
+    sum = num1 + num2;
+
+    // Displaying the result
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
